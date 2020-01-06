6189b2aefb0bbcb1419c31a7268c7575e9aaf818
wisscot <wisscot.register@gmail.com>
2020-01-06 15:51:39 -0500

    two pointers

--- Leetcode/0209.py
+++ Leetcode/0209.py
@@ -11,20 +11,25 @@ Solution 2. Two Pointers
   Basic idea: 
   use i, j as the sum range, if range[i to j] meets the target, then for increasing j', i' must be > i
   so we can use same direction two pointers
+  i is the master pointer - for loop
+  j is the slave pointer - while loop
 
 class Solution:
     def minSubArrayLen(self, s: int, nums: List[int]) -> int:
         # write your code here
         
-        # i and j are inclusive
-        i = 0
         res = float('inf')
         curr_sum = 0
-        for j in range(len(nums)):
-            curr_sum += nums[j]
-            while i <= j and curr_sum >= s:
-                res = min(res, j-i+1)
-                curr_sum -= nums[i]
-                i += 1
+        j = 0
+        for i in range(len(nums)):
+            # move j until cannot
+            while j < len(nums) and curr_sum < s:
+                curr_sum += nums[j]
+                j += 1
+            # save result
+            if curr_sum >= s:
+                res = min(res, j-i)
+            # move i
+            curr_sum -= s[i]
         
         return res if res<float('inf') else -1
